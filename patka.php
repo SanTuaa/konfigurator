07b2237fcc20d7ee7de74ce5080d6f05538b0098
Simon Ruzicka <63016579+ruzicka02@users.noreply.github.com>
2021-06-15 13:58:19 +0200

    Update patka.php
    GH username change

--- patka.php
+++ patka.php
@@ -3,12 +3,12 @@
 #uzavira div obsah, body a html?>
 		</div>
 		<div id="pata">
-			<p>KonfiguraTHOR&copy; 2021, <a href="https://github.com/SanTuaa/konfigurator/commits/main">verze 21/04.6</a></p>
+			<p>KonfiguraTHOR&copy; 2021, <a href="https://github.com/ruzicka02/konfigurator/commits/main">verze 21/04.6</a></p>
 			<p>Vytvořil <a href="mailto:sima.ruzicka@gmail.com">
 			Šimon Růžička</a>, Gymnázium Budějovická</p>
 			<p>Stránka byla vytvořena pro čistě nekomerční účely jako seminární práce s otevřeným zdrojovým kódem</p>
 			<div class="empty_smol"></div>
-			<a href="http://github.com/SanTuaa/konfigurator">Zdrojový kód na GitHubu</a>
+			<a href="http://github.com/ruzicka02/konfigurator">Zdrojový kód na GitHubu</a>
 		</div>
 	</body>
 </html>
